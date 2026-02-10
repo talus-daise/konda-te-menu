b14125b5a53cfe86654aad46972fb578032552c4
talus-yujiro <daise.talus@gmail.com>
2026-02-10 19:12:11 +0900

    Firebase Cloud Messagingを統合し、通知機能を追加。Supabaseクライアントを新規作成し、関連するスクリプトを整理。

--- script/todo.js
+++ script/todo.js
@@ -1,9 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const supabase = createClient(
-    "https://nnfvwpzwvscfpyrrsygt.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uZnZ3cHp3dnNjZnB5cnJzeWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNjgzMjIsImV4cCI6MjA4NTc0NDMyMn0.wqeKyvXjVKK8oo5CD09L8FvEH3H7rs2Xit-H4FG1HSc"
-);
+import { supabase } from "./supabase-client.js";
 
 async function fetchTodos() {
     const channel = supabase
